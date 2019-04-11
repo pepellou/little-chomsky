4761f951f3298034d60de1ee57d36dfee64746d7
Pepe Doval <pepellou@gmail.com>
2019-04-11 12:26:42 -0500

    Basic rules for greetings
    Simple exact match of input.
    Multiple random answers.
    Redirects.

--- Chomsky/Chomsky.php
+++ Chomsky/Chomsky.php
@@ -1,12 +1,74 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Chomsky;
 
 
-class Chomsky {
+final class Chomsky {
 
-    public static function talk($text) {
+    public static function learn($knowledge_file) : void
+    {
+        // TODO do
+    }
+
+    public static function talk($text) : string
+    {
+        foreach (self::$rules as $category => $rules) {
+            foreach ($rules as $rule) {
+                if (self::ruleApplies($text, $rule)) {
+                    return self::applyRule($text, $rule);
+                }
+            }
+        }
         return 'Non entendo o que queres dicir (' . $text . ')';
     }
+
+    private static function ruleApplies($text, $rule) : bool
+    {
+        return $text == $rule['input'];
+    }
+
+    private static function applyRule($text, $rule) : string
+    {
+        if (isset($rule['redirect'])) {
+            return self::talk($rule['redirect']);
+        }
+        if (isset($rule['answer'])) {
+            if (is_string($rule['answer'])) {
+                return $rule['answer'];
+            }
+            if (is_array($rule['answer'])) {
+                return $rule['answer'][rand(0, count($rule['answer']))];
+            }
+        }
+        return null;
+    }
+
+    private static $rules = [
+        'basic_conversation' => [
+            [
+                'input'  => 'Ola',
+                'answer' => [
+                    'Ola!',
+                    'Ola!!',
+                    'Ola, que tal? :-)',
+                    'Que tal?',
+                ]
+            ],
+            [
+                'input'    => 'Que tal?',
+                'redirect' => 'Ola'
+            ],
+            [
+                'input'    => 'Boas',
+                'redirect' => 'Ola'
+            ],
+            [
+                'input'    => 'Bo dia',
+                'redirect' => 'Ola'
+            ],
+        ]
+    ];
 
 }
