9248919b724876f5c1acf2a08027b1a1028a0e6a
Pepe Doval <pepellou@gmail.com>
2019-04-20 14:20:07 -0500

    Hide API key

--- console.php
+++ console.php
@@ -12,7 +12,6 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Chomsky\Chomsky;
-use Symfony\Component\Yaml\Yaml;
 
 Chomsky::learnFromKnowledgeFolder();
 
--- cron.php
+++ cron.php
@@ -14,9 +14,13 @@ $commands = [
 // Load composer
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Symfony\Component\Yaml\Yaml;
+
+$config = Yaml::parseFile('config.yaml');
+
 // Add you bot's API key and name
-$bot_api_key  = '877772552:AAGTxeFlJgnE4pFTfeAKlp3hqYDCUxynZaM';
-$bot_username = 'LittleChomskyBot';
+$bot_api_key  = $config['telegram']['api_key'];
+$bot_username = $config['telegram']['username'];
 
 // Define all IDs of admin users in this array (leave as empty array if not used)
 $admin_users = [
